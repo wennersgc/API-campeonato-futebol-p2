e86aaca4336a8a6c73027ddf66b7f9ed90624e0e
wenner-sanner <wennersanner@hotmail.com>
2018-07-05 13:17:07 -0300

    Atualizando valores na tabela jogador

--- src/main/resources/db/migration/mysql/V5__altera_tabela_jogador_2.sql
+++ src/main/resources/db/migration/mysql/V5__altera_tabela_jogador_2.sql
@@ -1,2 +1,7 @@
 ALTER TABLE jogador ADD numero_camisa int(11) NOT NULL AFTER altura;
 ALTER TABLE jogador ADD posicao VARCHAR(255) NOT NULL AFTER numero_camisa;
+
+update jogador set numero_camisa = 10 posicao = 'Meia esquerda' where id=1;
+update jogador set numero_camisa = 5 posicao = 'Volante' where id=2;
+update jogador set numero_camisa = 1 posicao = 'Goleiro' where id=3;
+
